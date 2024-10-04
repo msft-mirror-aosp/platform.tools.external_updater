5de25f92d3a7992f3982195381ec137d869471f0
Sadaf Ebrahimi <sadafebrahimi@google.com>
2024-10-04 19:52:56 +0000

    Add Kokoro build script and configuration
    The added text file is the a very small subset of external_updater
    compatible repositories under external. This is for testing purposes
    only.
    
    Bug: 365595416
    Test: TreeHugger
    Change-Id: I7cb562049d9ceb4e9561e6a3d688429cb49e503e

---
+++ kokoro/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+TOP=$(cd $(dirname $0)/../../.. && pwd)
+EXTERNAL_UPDATER=$(pwd)
+cd $TOP
+repo init -u sso://android/platform/manifest -b main --depth=1 < /dev/null
+repo sync -c
+source build/envsetup.sh
+lunch aosp_cf_x86_64_phone-trunk_staging-eng
+mm -j tools/external_updater
+cd $EXTERNAL_UPDATER
+input="compatible_repositories.txt"
+while IFS= read -r line; do
+    PROJ_PATH=$TOP/external/$line
+    $TOP/out/host/linux-x86/bin/external_updater check $PROJ_PATH
+done < "$input"
+trap 'rm -rf $TOP' EXIT
