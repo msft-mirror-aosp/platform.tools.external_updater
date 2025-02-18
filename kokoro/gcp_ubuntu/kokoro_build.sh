bb8b62b2624c38a1232d9cddfdffee1dfad93a34
Sadaf Ebrahimi <sadafebrahimi@google.com>
2025-02-18 21:20:05 +0000

    Only build external_updater
    Test: TreeHugger
    Change-Id: Ib726d700b1db35665747ad1fde14f350371df71c

--- kokoro/gcp_ubuntu/kokoro_build.sh
+++ kokoro/gcp_ubuntu/kokoro_build.sh
@@ -5,17 +5,23 @@ TOP=$(cd $(dirname $0)/../../../.. && pwd)
 EXTERNAL_UPDATER=$(pwd)/../..
 
 cd $TOP
+echo "Initializing Android tree and syncing"
 repo init -u https://android.googlesource.com/platform/manifest -b main --depth=1 < /dev/null
 repo sync -c
 
 source build/envsetup.sh
 lunch aosp_cf_x86_64_phone-trunk_staging-eng
-m
 
+cd $EXTERNAL_UPDATER
+echo "Building external_updater"
+mm -j
+
+echo "Checking compatible projects"
 input=$EXTERNAL_UPDATER/kokoro/gcp_ubuntu/"compatible_repositories.txt"
 
 while IFS= read -r line; do
     PROJ_PATH=$TOP/external/$line
+    echo "$PROJ_PATH"
     $TOP/out/host/linux-x86/bin/external_updater check $PROJ_PATH
 done < "$input"
 
